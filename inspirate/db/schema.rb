94730647a123d6184c88e7961635025ae5750160
Christopher Mamian <christophermamian@gmail.com>
2015-03-01 23:38:34 -0800

    add comments to posts

--- inspirate/db/schema.rb
+++ inspirate/db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302064332) do
+ActiveRecord::Schema.define(version: 20150302065429) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "link"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|

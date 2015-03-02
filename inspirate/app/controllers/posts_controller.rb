008596f926e5bcbccd540e956c62f97a09039d5c
Christopher Mamian <christophermamian@gmail.com>
2015-03-01 22:26:08 -0800

    Add create update destroy ability for posts

--- inspirate/app/controllers/posts_controller.rb
+++ inspirate/app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
@@ -24,9 +26,16 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@post.destroy
+		redirect_to root_path
 	end
 
 	private

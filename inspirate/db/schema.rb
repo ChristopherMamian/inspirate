ef0e47b6bed7a2f3f0190666df09fe87ef5cab40
Christopher Mamian <christophermamian@gmail.com>
2015-03-01 22:52:37 -0800

    add name to users

--- inspirate/db/schema.rb
+++ inspirate/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302063438) do
+ActiveRecord::Schema.define(version: 20150302064332) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150302063438) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

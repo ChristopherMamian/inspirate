860b73c603594ecf92f134d41a3db44a343efd23
Christopher Mamian <christophermamian@gmail.com>
2015-03-01 21:56:40 -0800

    updated post controller actions

--- inspirate/app/controllers/posts_controller.rb
+++ inspirate/app/controllers/posts_controller.rb
@@ -1,4 +1,41 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
+	end
+
+	def show
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :link, :description)
 	end
 end
